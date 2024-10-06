e1fb1a053057e9368f46e43a25a4428a714936bf
De vona C <devonacanada@gmail.com>
2024-10-06 17:04:05 -0400

    edited gitignore trying to run build

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,7 @@
 import inquirer from 'inquirer';
 import fs from 'fs';
 import path from 'path';
-import generateMarkdown from ('./utils/generateMarkdown.js');
+import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
 const questions = [
