c404a8529841ae2ed502216c59747bf866af959f
De vona C <devonacanada@gmail.com>
2024-10-07 22:50:36 -0400

    fix bug

---
+++ Develop/dist/ReadME.md
@@ -0,0 +1,27 @@
+# asd
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  ## Description
+  asd
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test](#test)
+  - [License](#license)
+  ## Installation
+  asdasd
+  ## Usage
+  asdadas
+  ## Contribution
+  asda
+  ## Test
+  asd
+  ## License
+  MIT License
+  ## Questions
+  - [GitHub](
+  asd)
+  - Email: asda
+  - [Link](
+  asd)
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,131 +1,88 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
-import fs, { writeFileSync } from 'fs';
+import fs from 'fs';
 import generateMarkdown from './utils/generateMarkdown.js';
+import path from 'path';
 // TODO: Create an array of questions for user input
-const questions = [
-  'What is the title of your project?',
-    'Please provide a description of your project?',
-    'Please provide installation instructions?',
-    'Please provide usage information?',
-    'Please provide contribution guidelines?',  
-    'Please provide test instructions?',
-    'Please select a license?',
-    'Please enter your GitHub username?',
-    'Please enter your email address?',
-    'Please provide a link to your github?'  
-];
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: questions[0],
-      name: 'title',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-    {
-      type: 'input',
-      message: questions[1],
-      name: 'description',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-    {
-      type: 'input',
-      message: questions[2],
-      name: 'installation',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-    {
-      type: 'input',
-      message: questions[3],
-      name: 'usage',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-    {
-      type: 'input',
-      message: questions[4],
-      name: 'contribution',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-    {
-      type: 'input',
-      message: questions[5],
-      name: 'test',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-    {
-      type: 'list',
-      message: questions[6],
-      name: 'license',
-      choices: ['MIT', 'Apache', 'GPL', 'BSD'],
-    },
-    {
-      type: 'input',
-      message: questions[7],
-      name: 'github',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-    {
-      type: 'input',
-      message: questions[8],
-      name: 'email',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-    {
-      type: 'input',
-      message: questions[9],
-      name: 'link',
-      validate: (value) => {if(value) return true; else return 'Please enter a value';},
-    },
-  ])
-  .then((response) => {
-    console.log(response);
-  });
- 
-//TODO: create a template with links to each section of the quetsions
-const template = `# ${data.title}
-## Description
-${data.description}
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [Contribution](#contribution)
-- [Test](#test)
-- [License](#license)
-- [Questions](#questions)
-## Installation
-${data.installation}
-## Usage
-${data.usage}
-## C ontribution
-${data.contribution}
-## Test
-${data.test}
-## License
-${data.license}
-## Questions
-- GitHub: [${data.github}](${data.link})
-- Email: ${data.email}`;
+
+const questions = [
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+        {
+            type: 'input',
+            message: 'Please provide a description of your project?',
+            name: 'description',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+        {
+            type: 'input',
+            message: 'Please provide installation instructions?',
+            name: 'installation',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+        {
+            type: 'input',
+            message: 'Please provide usage information?',
+            name: 'usage',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+        {
+            type: 'input',
+            message: 'Please provide contribution guidelines?',
+            name: 'contribution',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+        {
+            type: 'input',
+            message:   'Please provide test instructions?',
+            name: 'test',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+        {
+            type: 'list',
+            message: 'Please select a license?',
+            name: 'license',
+            choices: ['MIT', 'Apache', 'GPL', 'BSD'],
+        },
+        {
+            type: 'input',
+            message: 'Please enter your GitHub username?',
+            name: 'github',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+        {
+            type: 'input',
+            message: 'Please enter your email address?',
+            name: 'email',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+        {
+            type: 'input',
+            message: 'Please provide a link to your github?',
+            name: 'link',
+            validate: (value) => { if (value) return true; else return 'Please enter a value'; },
+        },
+    ];
    
 
 
 // TODO: Create a function to write README file in the newReademe.md file
-function writeToFile(fileName, data ) {
-let doc = createDoc(data);
-
-    fs.writeFile('newReadMe.md', doc, (err) =>
-        err ? console.log(err) : console.log('Success!')
-    );
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 
 //// TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((reponse) => {
-            console.log('Generating Professinal README.md File...');
-            writeFileSync("./Develop/newReadMe", generateMarkdown({...responses}));
-        });
+    inquirer.prompt(questions).then((response) => {
+        console.log('Generating Professinal README.md File...');
+        writeToFile("./dist/ReadME.md", generateMarkdown({ ...response }));
+    });
 }
 
 // Function call to initialize app
